78e8e4633fd64f4f1f986772963517a484bf8e5d
umbreon-nu <zoroark7@gmail.com>
2021-12-27 02:22:00 +0900

    商品ページの作成

---
+++ backend/resources/views/home.blade.php
@@ -0,0 +1,220 @@
+<html lang="ja">
+    <head>
+        <title>アイラブ評価 | フカタロウ</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <header id="ilovehyouka" class="bg-white sticky-top">
+        <div class="container">
+            <div class="col col-12 d-flex justify-content-between flex-column flex-md-row align-items-center">
+                <div class="col-9">
+                    <a href="/home">
+                        <img src="images/ilovehyouka.png" alt="アイラブ評価" height="85">
+                    </a>
+                </div>
+                <div class="col-3 d-none d-md-block d-print-block">
+                    <img src="images/logo.png" height="60">
+                </div>
+            </div>
+        </div>
+    </header>
+    <section>
+        <div class="card" style="max-width: 2000px;">
+            <img class="bd-placeholder-img bd-placeholder-img-lg card-img" width="100%" src="images/main.png">
+        </div>
+    </section>
+    <section class="bg-primary">
+        <div class="container py-3 text-white">
+            <h2 class="text-center mb-5 mt-5">
+                <span style="background: linear-gradient(transparent 85%, #ccffff 0%);">フカタロウ</span>
+                。癒しを与える電子負荷装置。
+            </h2>
+            <div class="row mb-5">
+                <div class="col col-12 col-md-6 d-flex justify-content-center mb-3 mb-md-0">
+                    <img width="80%" src="images/nukumori.jpg">
+                </div>
+                <div class="col col-12 col-md-6 mb-3 mb-md-0">
+                    <h4>木の温かみを疑似体験できる、唯一無二の評価機。</h4>
+                    <p>
+                        電子負荷装置から発生する「熱」は、しばしばネガティブなイメージとして捉えられます。<br>
+                        本製品は、「熱」を「木の温かみ」として再定義することで、普段の評価業務を良い気分で行えるようになります。<br>
+                        最高の評価ライフを、フカタロウと共に。
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bg-white">
+        <div class="container py-3">
+            <h2 class="text-center text-black mb-5 mt-5">
+                機能紹介
+            </h2>
+            <div class="row mt-5">
+                <div class="col col-12 col-md-4 mb-4">
+                    <h2 class="text-danger mb-3">- point 1.</h2>
+                    <div class="card border-white mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold">2端子同時負荷</h5>
+                            <p class="card-text">
+                                これ一台で、2つの端子に同時に負荷をかけることが可能です。
+                                <span style="background: linear-gradient(transparent 85%, #c93965 0%);">
+                                    電子負荷装置を2台設置して評価する必要はもうありません。<br>
+                                </span>
+                                また、それぞれの端子にかける負荷量を別々で設定できます。
+                            </p>
+                        </div>
+                        <img class="bd-placeholder-img card-img-bottom" width="100%" src="images/2in1.jpg">
+                    </div>
+                </div>
+                <div class="col col-12 col-md-4 mb-4">
+                    <h2 class="text-danger mb-3">- point 2.</h2>
+                    <div class="card border-white mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold">ロギング機能</h5>
+                            <p class="card-text">
+                                測定中のデータをWi-Fi接続、もしくはUSB接続にて出力し、記録できます。
+                                <span style="background: linear-gradient(transparent 85%, #c93965 0%);">
+                                    測定値のメモを取る作業はもう必要ありません。
+                                </span>
+                            </p>
+                        </div>
+                        <img class="bd-placeholder-img card-img-bottom" width="100%" src="images/logging.png">
+                    </div>
+                </div>
+                <div class="col col-12 col-md-4 mb-4">
+                    <h2 class="text-danger mb-3">- point 3.</h2>
+                    <div class="card border-white mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold">シンプルな操作方法</h5>
+                            <p>
+                                本製品は定電流負荷機能にフォーカスを当てた、シンプルな設計になっています。<br>
+                                ①電源ボタンを押し、②端子を接続して、③あとはツマミを回すだけ。
+                                <span style="background: linear-gradient(transparent 85%, #c93965 0%);">
+                                    装置を理解する手間も、手順書を作成する必要もありません。<br>
+                                </span>
+                            </p>
+                        </div>
+                        <img class="bd-placeholder-img car-img-bottom" width="100%" src="images/simple.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bg-success">
+        <div class="container py-3">
+            <h2 class="text-center text-white mb-5 mt-5">
+                お買い求めはこちら
+            </h2>
+            <div class="row mt-5 d-flex align-items-center">
+                <div class="col col-12 col-md-6 mb-6">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h4 class="card-title fw-bold text-center">アイラブ評価シリーズ - フカタロウ</h5>
+                        </div>
+                        <img class="bd-placeholder-img card-img-bottom" src="images/hukataro_detail.png">
+                    </div>
+                </div>
+                <div class="col col-12 col-md-6 mb-6">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-2">
+                                    <h5>型番 : </h5>
+                                </div>
+                                <div class="col-8">
+                                    <h5>ELD-SANY-MK Ⅱ</h5>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-2">
+                                    <h5>価格 : </h5>
+                                </div>
+                                <div class="col-8">
+                                    <h5>30,000 円</h5>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-2">
+                                    <h5>個数 : </h5>
+                                </div>
+                                <div class="col-4">
+                                    <input type="number" class="form-control" id="number" placeholder="1" min="1" max="10">
+                                </div>
+                                <div class="col-4">
+                                    <button type="submit" class="btn btn-primary text-nowrap">購入する</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bg-secondary">
+        <div class="container py-3">
+            <h2 class="text-center text-black mb-5 mt-5">
+                製品仕様
+            </h2>
+            <div class="col col-12 col-md-6 offset-md-3 mb-4">
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <th scope="row">負荷電流 設定可能範囲</th>
+                            <td>0.00 ~ 3.15 A</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">負荷電流 設定可能分解能</th>
+                            <td>0.01 A</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">推奨負荷電力</th>
+                            <td>各 15 W (Load1 / Load2)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">負荷接続端子</th>
+                            <td>電極ターミナル(バナナジャック) : 赤色 x2、黒色 x2</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">ホストインターフェース</th>
+                            <td>USB 2.0 Full-Speed</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">通信速度</th>
+                            <td>12 Mbps (Full-Speed)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">USBポート</th>
+                            <td>USB Standard B コネクター : x1</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">無線ネットワーク</th>
+                            <td>2GHz帯 : 802.11b/g/n</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">筐体</th>
+                            <td>材質 : 木材(桐)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">電源</th>
+                            <td>標準ACアダプタ 12 V / 2.5 A</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">ログ仕様</th>
+                            <td>
+                                記録間隔 : 180 s<br>
+                                記録内容 : 電圧, 電流, 電力 (Load1 / Load2)
+                            </td>
+                        </tr>
+                    </tdoby>
+                </table>
+            </div>
+        </div>
+    </section>
+    <footer class="container py-5">
+        <div class="row">
+            <div class="col-12 col-md-6 offset-md-3 text-center">
+                <h5>About</h5>
+                <small class="d-block mb-3 text-muted">Team S@NY - © 2021</small>
+            </div>
+        </div>
+    </footer>
+</html>
